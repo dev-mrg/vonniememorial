172c10d21fc08a8c83ab2bc7d0d672d7c589e736
dev-mrg <mick.groff@devmg.dev>
2020-09-01 04:41:36 -0600

    .butterflies

---
+++ dist/butterly.js
@@ -0,0 +1,11 @@
+function box1() {
+
+    var y = Math.floor(Math.random() * 10) * Math.floor(Math.random() * 43);
+    var z = Math.floor(Math.random() * 11) * Math.floor(Math.random() * 11) * 10;
+
+    document.getElementById("box1").style.top = y + "px";
+    document.getElementById("box1").style.left = z + "px";
+
+    document.getElementById("x-val").innerHTML = "TOP = " + y + " px";
+    document.getElementById("y-val").innerHTML = "LEFT = " + z + " px";
+};
--- dist/script.js
+++ dist/script.js
@@ -1,32 +1,32 @@
-jQuery("#banner").mousemove(
-    function(e) {
+// jQuery("#banner").mousemove(
+//     function(e) {
 
-        var offset = jQuery(this).offset();
-        var xPos = e.pageX - offset.left - 150;
-        var yPos = e.pageY - offset.top - 200;
+//         var offset = jQuery(this).offset();
+//         var xPos = e.pageX - offset.left - 150;
+//         var yPos = e.pageY - offset.top - 200;
 
-        var mouseXPercent = Math.round(xPos / jQuery(this).width() * 1000);
-        var mouseYPercent = Math.round(yPos / jQuery(this).height() * 100);
+//         var mouseXPercent = Math.round(xPos / jQuery(this).width() * 1000);
+//         var mouseYPercent = Math.round(yPos / jQuery(this).height() * 100);
 
-        jQuery(this).children('img').each(
-            function() {
-                var diffX = jQuery('#Parallax').width() - jQuery(this).width();
-                var diffY = jQuery('#Parallax').height() - jQuery(this).height();
+//         jQuery(this).children('img').each(
+//             function() {
+//                 var diffX = jQuery('#Parallax').width() - jQuery(this).width();
+//                 var diffY = jQuery('#Parallax').height() - jQuery(this).height();
 
-                var myX = diffX * (mouseXPercent / 800);
+//                 var myX = diffX * (mouseXPercent / 800);
 
-                var myY = diffY * (mouseYPercent / 1080);
+//                 var myY = diffY * (mouseYPercent / 1080);
 
 
-                var cssObj = {
-                    'left': myX + 'px',
-                    'top': myY + 'px'
-                }
+//                 var cssObj = {
+//                     'left': myX + 'px',
+//                     'top': myY + 'px'
+//                 }
 
-                jQuery(this).animate({ left: -myX, top: -myY }, { duration: 590, queue: false, easing: 'linear' });
+//                 jQuery(this).animate({ left: -myX, top: -myY }, { duration: 590, queue: false, easing: 'linear' });
 
-            }
-        );
+//             }
+//         );
 
-    }
-);
+//     }
+// );
